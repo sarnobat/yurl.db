9d537b9d2b0c80c7d44951c3ade4ff091ea91dcf
sarnobat <ss401533@gmail.com>
2013-08-21 12:52:54 -0700

    Initial commit

--- webserver_json.groovy
+++ webserver_json.groovy
@@ -1,180 +1,95 @@
-
-import java.io.DataInputStream;
-import java.net.URL;
-import java.net.URLConnection;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import org.neo4j.graphdb.*;
-import org.neo4j.graphdb.GraphDatabaseService;
-import org.neo4j.graphdb.Node;
-import org.neo4j.graphdb.factory.GraphDatabaseFactory;
-import org.neo4j.tooling.GlobalGraphOperations;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import org.apache.http.client.utils.URLEncodedUtils;
-import org.json.JSONObject;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
-import org.apache.commons.io.*;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.Transaction;
+import org.neo4j.graphdb.factory.GraphDatabaseFactory;
+import org.neo4j.tooling.GlobalGraphOperations;
+
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
-
- 
 class MyHandler implements HttpHandler {
-	public Map<String, String> getQueryMap(String query)  
-	{  
-		Pattern pattern = Pattern.compile("/\\?.*");
-
-		Matcher matcher = pattern.matcher(query);
-		String[] params = query.split("&");  
-		Map<String, String> map = new HashMap<String, String>();  
-		for (String param : params)  
-		{  
-			String name = param.split("=")[0];  
-			String value = param.split("=")[1];  
-			map.put(name, value);  
-			println(name + " :: " + value);
-		}  
-		return map;  
-	}  
-	
-	public String getTitle( URL theURL )
-	{
-		String startTag = "<title>";
-		String endTag = "</title>";
-		int startTagLength = startTag.length();
-		
-		BufferedReader bufReader;
-		String line;
-		boolean foundStartTag = false;
-		boolean foundEndTag = false;
-		int startIndex, endIndex;
-		String title = "";
-		
-		try
-		{
-		  //open file
-		  bufReader = new BufferedReader( new InputStreamReader(theURL.openStream()) );
-		  
-		  //read line by line
-		  while( (line = bufReader.readLine()) != null && !foundEndTag)
-		  {
-			//System.out.println(line);
-		  
-			//search for title start tag (convert to lower case before searhing)
-			if( !foundStartTag && (startIndex = line.toLowerCase().indexOf(startTag)) != -1 )
-			{
-			  foundStartTag = true;
-			}
-			else
-			{
-			  //else copy from start of string
-			  startIndex = -startTagLength;
-			}
-			
-			//search for title start tag (convert to lower case before searhing)
-			if( foundStartTag && (endIndex = line.toLowerCase().indexOf(endTag)) != -1 )
-			{
-			  foundEndTag = true;
-			}
-			else
-			{
-			  //else copy to end of string
-			  endIndex = line.length();
-			}
-			
-			//extract title field
-			if( foundStartTag || foundEndTag )
-			{
-			  //System.out.println("foundStartTag="+foundStartTag);
-			  //System.out.println("foundEndTag="+foundEndTag);
-			  //System.out.println("startIndex="+startIndex);
-			  //System.out.println("startTagLength="+startTagLength);
-			  //System.out.println("endIndex="+endIndex);
-			
-			  title += line.substring( startIndex + startTagLength, endIndex );
-			}
-		  }
-		  
-		  //close the file when finished
-		  bufReader.close();
-		  
-		  //output the title
-		  if( title.length() > 0 )
-		  {
-			System.out.println("Title: "+title);
-		  }
-		  else
-		  {
-			System.out.println("No title found in document.");
-		  }
-		  return title;
-		  
-		}
-		catch( IOException e )
-		{
-		  System.out.println( "GetTitle.GetTitle - error opening or reading URL: " + e );
-		}
-		return "";
-	}
 
 	public void handle(HttpExchange t) throws IOException {
 		JSONObject json = new JSONObject();
-		String query = t.getRequestURI();
-		Map<String, String> map = getQueryMap(query);  
-		String value = map.get("/?param1");
-		json.put("myKey",value);
-		println('Request headers: ' + t.getRequestHeaders());
-		println('Request URI:' + t.getRequestURI());
-		println('value: ' + value);
-		json.put("foo","bar");
-		t.getResponseHeaders().add("Access-Control-Allow-Origin","*");
+		String query = t.getRequestURI().toString();
+		Map<String, String> map = getQueryMap(query);
+		String httpUrl = map.get("/?param1");
+		try {
+			json.put("myKey", httpUrl);
+		} catch (JSONException e) {
+			e.printStackTrace();
+			return;
+		}
+		System.out.println("Request headers: " + t.getRequestHeaders());
+		System.out.println("Request URI:" + t.getRequestURI());
+		System.out.println("value: " + httpUrl);
+		try {
+			json.put("foo", "bar");
+		} catch (JSONException e) {
+			e.printStackTrace();
+			return;
+		}
+		t.getResponseHeaders().add("Access-Control-Allow-Origin", "*");
 		t.getResponseHeaders().add("Content-type", "application/json");
 		t.sendResponseHeaders(200, json.toString().length());
-		
-		
-		//
-		// Get page title
-		//
-		println("1");
 
-		URL url = new URL(value);
-		String title = getTitle(url);
-		println('title:: ' + title);
-		
+		String title = getTitle(new URL(httpUrl));
+		System.out.println("title:: " + title);
+
 		//
 		// Graph persistence
 		//
-		
-		GraphDatabaseService graphDb = new GraphDatabaseFactory().newEmbeddedDatabase("yurl.db");
-		
-		
-		//
-		// Create node
-		//
-		
+
+		writeLink(httpUrl, title);
+
+		OutputStream os = t.getResponseBody();
+		os.write(json.toString().getBytes());
+		os.close();
+	}
+
+	public void writeLink(String value, String title) {
+		GraphDatabaseService graphDb = new GraphDatabaseFactory()
+				.newEmbeddedDatabase("yurl.db");
 		Transaction tx = graphDb.beginTx();
-		
-		try
-		{
+		try {
 			Node firstNode = graphDb.createNode();
-			firstNode.setProperty( "title", title);	
-			firstNode.setProperty( "url", value);			
+			firstNode.setProperty("title", title);
+			firstNode.setProperty("url", value);
 			tx.success();
-		}
-		finally
-		{
+		} finally {
 			tx.finish();
 		}
-		
-		
-		//
-		// Print nodes
-		//
-		
+		printAllNodes(title, graphDb);
+		graphDb.shutdown();
+	}
+
+	public void printAllNodes(String title, GraphDatabaseService graphDb) {
 		Iterable<Node> allNodes = GlobalGraphOperations.at(graphDb).getAllNodes();
 		for (final Node node : allNodes) {
 			if (node.hasProperty("title")) {
@@ -184,20 +99,53 @@ class MyHandler implements HttpHandler {
 			if (node.hasProperty("url")) {
 				name = (String) node.getProperty("url");
 			}
-		
+
 			System.out.println("\"" + title + "\",\"" + name + "\"");
 			System.out.println(name);
 			System.out.println();
 		}
-		graphDb.shutdown();
+	}
 
-		OutputStream os = t.getResponseBody();
-		os.write(json.toString().getBytes());
-		os.close();
+	public String getTitle(final URL url) {
+		String title = "";
+		ExecutorService service = Executors.newFixedThreadPool(2);
+		Collection<Callable<String>> tasks = new ArrayList<Callable<String>>();
+		Callable<String> callable = new Callable<String>() {
 
+			@Override
+			public String call() throws Exception {
+				Document doc = Jsoup.connect(url.toString()).get();
+				return doc.title();
+
+			}
+		};
+		tasks.add(callable);
+		try {
+			List<Future<String>> taskFutures = service
+					.invokeAll(tasks, 3000L, TimeUnit.SECONDS);
+			title = taskFutures.get(0).get();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} catch (ExecutionException e) {
+			e.printStackTrace();
+		}
+		return title;
+	}
+
+	private Map<String, String> getQueryMap(String query) {
+		// Pattern pattern = Pattern.compile("/\\?.*");
+		// Matcher matcher = pattern.matcher(query);
+		String[] params = query.split("&");
+		Map<String, String> map = new HashMap<String, String>();
+		for (String param : params) {
+			String name = param.split("=")[0];
+			String value = param.split("=")[1];
+			map.put(name, value);
+			System.out.println(name + " :: " + value);
+		}
+		return map;
 	}
 }
-    
 HttpServer server = HttpServer.create(new InetSocketAddress(4444), 0);
 server.createContext("/", new MyHandler());
 server.setExecutor(null); // creates a default executor
