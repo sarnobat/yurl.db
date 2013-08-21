74ac47b26c7bd913298496c10f7e557dac7b2ff2
sarnobat <ss401533@gmail.com>
2013-08-21 12:52:54 -0700

    Initial commit

--- webserver_json.groovy
+++ webserver_json.groovy
@@ -1,3 +1,8 @@
+import org.neo4j.graphdb.*;
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.factory.GraphDatabaseFactory;
+import org.neo4j.tooling.GlobalGraphOperations;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import org.apache.http.client.utils.URLEncodedUtils;
@@ -25,6 +30,7 @@ class MyHandler implements HttpHandler {
 			String name = param.split("=")[0];  
 			String value = param.split("=")[1];  
 			map.put(name, value);  
+			println(name + " :: " + value);
 		}  
 		return map;  
 	}  
@@ -33,18 +39,74 @@ class MyHandler implements HttpHandler {
 		JSONObject json = new JSONObject();
 		String query = t.getRequestURI();
 		Map<String, String> map = getQueryMap(query);  
-		String  value = map.get("param1");
+		String value = map.get("/?param1");
 		json.put("myKey",value);
 		println('Request headers: ' + t.getRequestHeaders());
-		println('Request URI' + t.getRequestURI());
+		println('Request URI:' + t.getRequestURI());
 		println('value: ' + value);
 		json.put("foo","bar");
 		t.getResponseHeaders().add("Access-Control-Allow-Origin","*");
 		t.getResponseHeaders().add("Content-type", "application/json");
 		t.sendResponseHeaders(200, json.toString().length());
+		
+		println('1');
+		//
+		// Graph persistence
+		//
+		
+		GraphDatabaseService graphDb = new GraphDatabaseFactory().newEmbeddedDatabase("yurl.db");
+		
+		
+		println('2');
+		//
+		// Create node
+		//
+		
+		Transaction tx = graphDb.beginTx();
+		
+		println('2.1');
+		try
+		{
+			Node firstNode = graphDb.createNode();
+			println('2.2 - ' + value);
+			firstNode.setProperty( "url", value);			
+			println('2.3');
+			// Updating operations go here
+			
+			println('2.4');
+			tx.success();
+			
+			println('3');
+		}
+		finally
+		{
+			tx.finish();
+		}
+		
+		
+		//
+		// Print nodes
+		//
+		
+		Iterable<Node> allNodes = GlobalGraphOperations.at(graphDb).getAllNodes();
+		for (final Node node : allNodes) {
+			String title = "";
+			if (node.hasProperty("title")) {
+				title = (String) node.getProperty("title");
+			}
+			String name = "";
+			if (node.hasProperty("url")) {
+				name = (String) node.getProperty("url");
+			}
+		
+			System.out.println("\"" + title + "\",\"" + name + "\"");
+			System.out.println(name);
+			System.out.println();
+		}
 		OutputStream os = t.getResponseBody();
 		os.write(json.toString().getBytes());
 		os.close();
+
 	}
 }
     
